6dbc2f147407914b954955ac3f41233087b08ef6
vrushti-mody <vrushtimody6@gmail.com>
2020-12-02 01:20:00 +0530

    Fixed bugs

--- routes/main.js
+++ routes/main.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index',{title: "My Express APp"});
 });
 
 // Get register Page
