979f50de66ac7264d0884a3368dfae4b8beef132
V2dha <rvividha@gmail.com>
2020-12-07 01:04:02 +0530

    merged both thetwo models

--- flask-api/app.py
+++ flask-api/app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, jsonify
+from flask_cors import CORS
 from predict import *
+from caption import *
 import warnings
 warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello():
@@ -20,6 +23,19 @@ def upload_file():
 			'description' : caption
 		}
 	return render_template('index.html', results = result_dic)
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    # GET request
+    if request.method == 'GET':
+        return jsonify({"hello":"World"}) 
+    # POST request
+    if request.method == 'POST':
+        data = request.get_json()  # parse as JSON
+        user_content = data["data"]
+        result = sentiment_predict(user_content)
+        return jsonify(result),200
+
 
 if __name__ == '__main__':
 	app.run(debug = True)
